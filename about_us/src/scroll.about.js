08ad029ce2a61124e457deaae580cf1aaef87faa
FiGo123 <filipgolovicc@gmail.com>
2018-11-29 15:36:15 +0100

    Adds final version of About us page, without header and footer

---
+++ about_us/src/scroll.about.js
@@ -0,0 +1,57 @@
+/*Handling bounce which is triggered by scrolling. Without this function
+ onScroll function would be called much more times than we need */ 
+/*function debounce(func, wait = 20, immediate = true) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};*/
+const ourMission = document.getElementById('our-mission');
+const sectionB1 = document.getElementById('section-b1');
+const sectionB2 = document.getElementById('gri');
+const sliderTitle = document.getElementById('slider-title');
+const sectionD = document.getElementById('section-d1');
+const sectionE1 = document.getElementById('section-e1');
+const sectionE2 = document.getElementById('section-e2');
+
+
+function checkSlide(e){
+    console.log(window.scrollY);
+    if(window.scrollY > 0){
+        ourMission.classList.add('fade-in');
+    }
+    if(window.scrollY > 190){
+        sectionB1.classList.add('fade-in');
+        sectionB2.classList.add('img-animationRL');
+    }
+    if(window.scrollY > 579){
+        sliderTitle.classList.add('fade-in');
+        sliderTitle.classList.add('img-animationLR');
+    }
+    if(window.scrollY > 1425){
+        sectionD.classList.add('fade-in');
+       
+    }
+    if(window.scrollY > 2548){
+        sectionE1.classList.add('fade-in');
+        sectionE1.classList.add('img-animationLR');
+        sectionE2.classList.add('fade-in');
+        sectionE2.classList.add('img-animationRL');
+       
+    }
+    
+}
+
+window.addEventListener('scroll', (checkSlide));
+
+
+
+
