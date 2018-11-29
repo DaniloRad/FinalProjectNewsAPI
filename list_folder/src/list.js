f9bd6831e4e1870417c4187ea422b756cf7f8017
LukaRaicevic <luka.raicevic888@gmail.com>
2018-11-29 17:20:48 +0100

    added search algorithm

--- list_folder/src/list.js
+++ list_folder/src/list.js
@@ -19,39 +19,129 @@ const sport = "https://newsapi.org/v2/everything?q=sport&pagesize=100&apiKey=872
 const technology = "https://newsapi.org/v2/everything?q=technology&pagesize=100&apiKey=8720fdbbd7504665a4e56dfa042a5d4c";
 const science = "https://newsapi.org/v2/everything?q=science&pagesize=100&apiKey=8720fdbbd7504665a4e56dfa042a5d4c";
 
-let getWhole = [first = [], second = [], third = [], fourth = [], fifth = []];
+let getWhole = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
 let getNewsArr = [];
+let filterArr = new Array(100);
+let tempData = [];
+let filterWholeArr = [];
 let pageCounter = 1;
 let checkUrl = "general";
 
 function fillNewsArr(data, br, i, counter) {
+    if(data.articles[br+counter].author === null) {data.articles[br+counter].author = "Unknown"};
     getWhole[i][br] = {
         title: data.articles[br+counter].title,
         img: data.articles[br+counter].urlToImage,
         author: data.articles[br+counter].author,
         content: data.articles[br+counter].content,
-        date: data.articles[br+counter].publishedAt.split("T")[0],
-        link: data.articles[br+counter].url
+        date: data.articles[br+counter].publishedAt.split("T")[0]
     }
 }
 
 function setDataInHtml(data, pageCounter) {
     let counter = 0;
-    for(let i = 0; i < 5; i++) {
+    for(let i = 0; i < 20; i++) {
         for(let br = 0; br < 5; br++) {
             $class("img"+(br+1))[0].innerHTML = "";
             $class("info"+(br+1))[0].innerHTML = "";
             fillNewsArr(data, br, i, counter);
-            if(getWhole[i][pageCounter-1].author === null) {getWhole[i][br].author = "Unknown"};
-            $class("img"+(br+1))[0].insertAdjacentHTML("afterbegin", "<img src="+getWhole[pageCounter-1][br].img+" alt="+"News"+i+" Image"+">");
+            $class("img"+(br+1))[0].insertAdjacentHTML("afterbegin", "<img src="+getWhole[pageCounter-1][br].img+" alt="+"News"+br+" Image"+">");
             $class("info"+(br+1))[0].insertAdjacentHTML("afterbegin", "<p>Title: "+getWhole[pageCounter-1][br].title+"</p> <br> <p>Author: "+getWhole[pageCounter-1][br].author+"</p> <br> <p>Date: "+getWhole[pageCounter-1][br].date+"</p>");
         }
         counter += 5;
     }
+    fillFilterArr();
+}
+
+function fillFilterArr() {
+    let counter = 0;
+    for(let i = 0; i < 20; i++) {
+        for(let j = 0; j < 5; j++) {
+            filterArr[counter] = getWhole[i][j];
+            counter++;
+        }
+    }
+}
+
+function whichBtn(elem, counter, input, i) {
+    if(elem.toLowerCase().includes(input)) {
+        tempData[counter] = filterArr[i];
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function filter() {
+    event.preventDefault();
+    tempData = [];
+    let userInp = $class("search-bar")[0].value.toLowerCase();
+    let titleBtn = $class("title-btn")[0];
+    let authorBtn = $class("author-btn")[0];
+    let counter = 0;
+    for(let i = 0; i < filterArr.length; i++) {
+        if(userInp === "") {
+            return;
+        } 
+        if(titleBtn.checked) {
+            if(whichBtn(filterArr[i].title, counter, userInp, i)) {
+                counter++;
+            }
+            checkUrl = "title";
+        } else if(authorBtn.checked) {
+            if(whichBtn(filterArr[i].author, counter, userInp, i)) {
+                counter++;
+            }
+            checkUrl = "author";
+        } else {
+            if(whichBtn(filterArr[i].date, counter, userInp, i)) {
+                counter++;
+            }
+            checkUrl = "date";
+        }
+    }
+    console.log(tempData);
+    if(tempData.length === 0) {
+        return;
+    }
+    tempToWhole();
+    setFilterArr();
+}
 
+function tempToWhole() {
+    let counter =  0;
+    filterWholeArr = [];
+    let num = Math.ceil(tempData.length/5);
+    if(num > 5) {
+        num = 5;
+    }
+    for(let i = 0; i < num; i++) {
+        let arr = [];
+        for(let j = 0; j < 5; j++) {
+            if(tempData[counter] !== undefined) {
+                arr[j] = tempData[counter];
+            }
+            counter++;
+        }
+        filterWholeArr.push(arr);
+    }
+    console.log(filterWholeArr);
+}
+
+function setFilterArr() {
+    let num = Math.ceil(tempData.length/5);
+    for(let br = 0; br < 5; br++) { 
+        if(pageCounter > num) {
+            alert("Nema vise stranica!");
+            break;
+        }
+        $class("img"+(br+1))[0].innerHTML = "";
+        $class("info"+(br+1))[0].innerHTML = "";
+        if(filterWholeArr[pageCounter-1][br] !== undefined) {
+            $class("img"+(br+1))[0].insertAdjacentHTML("afterbegin", "<img src="+filterWholeArr[pageCounter-1][br].img+" alt="+"News"+br+" Image"+">");
+            $class("info"+(br+1))[0].insertAdjacentHTML("afterbegin", "<p>Title: "+filterWholeArr[pageCounter-1][br].title+"</p> <br> <p>Author: "+filterWholeArr[pageCounter-1][br].author+"</p> <br> <p>Date: "+filterWholeArr[pageCounter-1][br].date+"</p>");
+        }
+    }
 }
 
 function fetchIt(url, pageCounter) {
@@ -97,7 +187,9 @@ $class("science")[0].addEventListener("click", function() {
     $class("prev")[0].style.visibility = "";
     $class("next")[0].style.visibility = "";
 });
-//$class("submit")[0].addEventListener("click",);
+$class("submit")[0].addEventListener("click", function() {
+    filter();
+});
 
 function pages() {
     if(event.target.innerHTML === "Prev") {
@@ -125,13 +217,16 @@ $class("pages-inner")[0].addEventListener("click", function() {
     pages();
     if(checkUrl === "general") {
         fetchIt(general, pageCounter)
-    } else if(checkUrl = "sport") {
+    } else if(checkUrl === "sport") {
         fetchIt(sport, pageCounter);
-    } else if(checkUrl = "technology") {
+    } else if(checkUrl === "technology") {
         fetchIt(technology, pageCounter);
-    } else {
+    } else if(checkUrl === "science") {
         fetchIt(science, pageCounter);
+    } else {
+        setFilterArr();
     }
-})
+    console.log(checkUrl)
+});
 
 firstFetch();
